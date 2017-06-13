ab6e01f3cffe39f13eb0d2e94946f2930b9c3494
Nelson Moller <moller_clara.nelson@uqam.ca>
2017-06-13 16:14:28 -0400

    Une augmentation des resources améne une nette amélioration de la performance.

--- tests/2017-06-08/create-secrets.sh
+++ tests/2017-06-08/create-secrets.sh
@@ -2,8 +2,9 @@
 #
 #
 
-sed "s/DB_USERNAME/$( echo $1 |base64 -w0)/g" secrets-moodle01.tmpl.yml | \
-sed "s/DB_PASSWORD/$( echo $2 |base64 -w0)/g" | \
-sed "s/DB_HOST/$( echo $3 |base64 -w0)/g" | \
-sed "s/DB_NAME/$( echo $4 |base64 -w0)/g" | \
+#tr -d '\n' pour ne pas avoir un saut de ligne.
+sed "s/DB_USERNAME/$( echo $1 |base64 -w0|tr -d '\n')/g" secrets-moodle01.tmpl.yml | \
+sed "s/DB_PASSWORD/$( echo $2 |base64 -w0|tr -d '\n')/g" | \
+sed "s/DB_HOST/$( echo $3 |base64 -w0|tr -d '\n')/g" | \
+sed "s/DB_NAME/$( echo $4 |base64 -w0|tr -d '\n')/g" | \
 cat -
