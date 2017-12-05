dc01428104bbbb11c9e1ba6fdf0ed9dba48d3675
Nelson Moller <moller_clara.nelson@uqam.ca>
2017-12-05 15:15:40 -0500

    On ne veut pas de saut de lignes dans les secrets.

--- tests/siad/create-secrets.sh
+++ tests/siad/create-secrets.sh
@@ -3,8 +3,8 @@
 #
 
 #tr -d '\n' pour ne pas avoir un saut de ligne.
-sed "s/DB_USERNAME/$( echo $1 |base64 -w0|tr -d '\n')/g" secrets-moodle01.tmpl.yml | \
-sed "s/DB_PASSWORD/$( echo $2 |base64 -w0|tr -d '\n')/g" | \
-sed "s/DB_HOST/$( echo $3 |base64 -w0|tr -d '\n')/g" | \
-sed "s/DB_NAME/$( echo $4 |base64 -w0|tr -d '\n')/g" | \
+sed "s/DB_USERNAME/$( echo -n $1 |base64 -w0|tr -d '\n')/g" secrets-moodle01.tmpl.yml | \
+sed "s/DB_PASSWORD/$( echo -n $2 |base64 -w0|tr -d '\n')/g" | \
+sed "s/DB_HOST/$( echo -n $3 |base64 -w0|tr -d '\n')/g" | \
+sed "s/DB_NAME/$( echo -n $4 |base64 -w0|tr -d '\n')/g" | \
 cat -
